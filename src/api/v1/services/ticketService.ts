d2cf17a2749cc4603b84e3bd41b6ad00944a1195
IvanBanal <ibanal@rrc.ca>
2026-02-07 16:23:32 -0600

    Adding Ticket interface and its documentation.

--- src/api/v1/services/ticketService.ts
+++ src/api/v1/services/ticketService.ts
@@ -1,8 +1,15 @@
+import { tickets } from "src/data/tickets";
+
+/**
+ * Interface representing the support ticket.
+ */
 export interface Ticket {
     id: number;
     title: string;
     description: string;
+    // 'critical', 'high', 'medium', 'low'.
     priority: string;
+    // 'open', 'in-progress', 'resolved'.
     status: string;
     createdAt: string;
 }
