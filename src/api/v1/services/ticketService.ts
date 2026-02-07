a30010e2fa7a338c77ec1a96a5509b8686d52489
IvanBanal <ibanal@rrc.ca>
2026-02-07 16:36:06 -0600

    Adding ageInDays function for calculating the age of the ticket in days.

--- src/api/v1/services/ticketService.ts
+++ src/api/v1/services/ticketService.ts
@@ -23,3 +23,22 @@ const baseScores: Record<string, number> = {
     low: 10
 };
 
+/**
+ * Calculate the age of the ticket in days.
+ * @param date ISO date string of creation.
+ * @returns Number of days since the ticket was created.
+ */
+const ageInDays = (date: string) =>
+    /**
+     * Math.floor rounds down to the nearest whole number of days.
+     * Date.now returns current timestamp in milliseconds (right now).
+     * new Date(date) converts a date string into a JavaScript Date object.
+     * .getTime() returns the date in milliseconds since midnight, 
+     * January 1, 1970 UTC.
+     * Dividng by 86400000 since there are 86400000 milliseconds in a day.
+     */ 
+    Math.floor((Date.now() - new Date(date).getTime()) / 86400000);
+
+
+
+
