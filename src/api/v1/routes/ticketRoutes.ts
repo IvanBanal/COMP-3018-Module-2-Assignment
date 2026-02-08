039d020255eb6420a81afc340068951c3ebefab4
IvanBanal <ibanal@rrc.ca>
2026-02-08 17:45:36 -0600

    Adding some documentation for ticketRoutes.ts.

--- src/api/v1/routes/ticketRoutes.ts
+++ src/api/v1/routes/ticketRoutes.ts
@@ -3,11 +3,14 @@ import * as controller from "../controllers/ticketController"
 
 const router = Router();
 
+// CRUD endpoints (/api/v1/ticket).
 router.post('/', controller.createTicket);
 router.get('/', controller.getAllTickets);
 router.get('/:id', controller.getTicketById);
 router.put('/:id', controller. updateTicket);
 router.delete('/:id', controller.deleteTicket);
+
+// Urgency endpoint.
 router.get('/:id/urgency', controller.getTicketWithUrgency);
 
 export default router;
