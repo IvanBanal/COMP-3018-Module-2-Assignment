8bdf366c422a151064e1b3a3f4d80faaa5c926d5
IvanBanal <ibanal@rrc.ca>
2026-02-08 16:49:08 -0600

    Adding createTicket controller for validating user inputs and sending responses back.

--- src/api/v1/controllers/ticketController.ts
+++ src/api/v1/controllers/ticketController.ts
@@ -1,6 +1,29 @@
 import { Request, Response } from "express";
 import * as service from "../services/ticketService";
+import { HTTP_STATUS } from "../../../constants/httpConstants";
 
 // These are arrays representing allowed priority levels and ticket statuses.
 const priorities = ["critical", "high", "medium", "low"];
 const statuses = ["open", "in-progress", "resolved"];
+
+// Create a new ticket.
+export const createTicket = (req: Request, res: Response) => {
+    const { title, description, priority} = req.body;
+
+    // Edge cases validations.
+    if (!title) {
+        res.status(HTTP_STATUS.BAD_REQUEST).json({ message: "Missing required field: title" });
+        return;
+    }
+    if (!description) {
+       res.status(HTTP_STATUS.BAD_REQUEST).json({ message: "Missing required field: description" });
+       return; 
+    } 
+    if (!priorities.includes(priority)) {
+        res.status(HTTP_STATUS.BAD_REQUEST).json({ message: "Invalid priority. Must be one of: critical, high, medium, low" });
+        return;
+    }
+
+    const ticket = service.createTicket({ title, description, priority });
+    res.status(HTTP_STATUS.CREATED).json(ticket);
+}
